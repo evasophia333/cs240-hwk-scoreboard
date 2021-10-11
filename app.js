a59e77ee696d1ff24a843b675dbb23985bc0c279
Eva Sophia Shimanski <eshimanski@pugetsound.edu>
2021-10-11 14:55:38 -0700

    changed classes and id's for the away and home sections

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 /**
  * Scoreboard Homework
- * @author 
+ * @author Eva Sophia Shimanski
  */
 
-let node1 = document.querySelector('.top1');  // select the element 
-let node2 = document.querySelector('.top2');  // select the element 
-let node3 = document.querySelector('.top3');  // select the element 
-let node4 = document.querySelector('.top4');  // select the element 
-let node5 = document.querySelector('.top5');  // select the element 
-let node6 = document.querySelector('.top6');  // select the element 
-let node7 = document.querySelector('.top7');  // select the element 
-let result = document.querySelector('.top8');  // select the element 
-let bottom1 = document.querySelector('.bottom1');  // select the element 
-let bottom2 = document.querySelector('.bottom2');  // select the element 
-let bottom3 = document.querySelector('.bottom3');  // select the element 
-let bottom4 = document.querySelector('.bottom4');  // select the element 
-let bottom5 = document.querySelector('.bottom5');  // select the element 
-let bottom6 = document.querySelector('.bottom6');  // select the element 
-let bottom7 = document.querySelector('.bottom7');  // select the element 
-let bottomresult = document.querySelector('.bottom8');  // select the element 
+let node1 = document.querySelector('#top1');  // select the element 
+let node2 = document.querySelector('#top2');  // select the element 
+let node3 = document.querySelector('#top3');  // select the element 
+let node4 = document.querySelector('#top4');  // select the element 
+let node5 = document.querySelector('#top5');  // select the element 
+let node6 = document.querySelector('#top6');  // select the element 
+let node7 = document.querySelector('#top7');  // select the element 
+let result = document.querySelector('#top8');  // select the element 
+let bottom1 = document.querySelector('#bottom1');  // select the element 
+let bottom2 = document.querySelector('#bottom2');  // select the element 
+let bottom3 = document.querySelector('#bottom3');  // select the element 
+let bottom4 = document.querySelector('#bottom4');  // select the element 
+let bottom5 = document.querySelector('#bottom5');  // select the element 
+let bottom6 = document.querySelector('#bottom6');  // select the element 
+let bottom7 = document.querySelector('#bottom7');  // select the element 
+let bottomresult = document.querySelector('#bottom8');  // select the element 
 
-let addition = node1.innerHTML + node2.innerHTML + node3.innerHTML + node4.innerHTML + node5.innerHTML + node6.innerHTML + node7.innerHTML;
 node1.innerHTML = 1;
 node2.innerHTML = 2;
 bottom1.innerHTML = 3;
